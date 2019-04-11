22125f248de6e3907c76a161a8a91e02c78e9204
Lwando92 <lwando@kineticskunk.com>
2019-04-11 13:12:10 +0200

    poi

--- application.py
+++ application.py
@@ -2,6 +2,6 @@
 from src.flaskbasic import application ,db
 from src.flaskbasic.form import *
 from src.flaskbasic.models import *
-db.create_all()
+
 if __name__ == '__main__':
 	application.run(debug=True)
