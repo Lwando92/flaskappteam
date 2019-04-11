aedd2aba15a8a6cf5bab8dd6942064301cfc0ba1
Lwando92 <lwando@kineticskunk.com>
2019-04-11 13:50:23 +0200

    database

--- application.py
+++ application.py
@@ -2,6 +2,6 @@
 from src.flaskbasic import application ,db
 from src.flaskbasic.form import *
 from src.flaskbasic.models import *
-
+from src.flaskbasic.db import *
 if __name__ == '__main__':
 	application.run(debug=True)
