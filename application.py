f0acc5e7eea74ba66e82ad54be0fd374311cdac5
Lwando92 <lwando@kineticskunk.com>
2019-04-11 12:42:46 +0200

    db

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
+
 from src.flaskbasic import application ,db
 from src.flaskbasic.form import *
-
+from src.flaskbasic.models import *
+db.create_all()
 if __name__ == '__main__':
 	application.run(debug=True)
--- src/flaskbasic/database.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from src.flaskbasic import application
-
-db = SQLAlchemy(application)
-
-from src.flaskbasic.wsgi import *
